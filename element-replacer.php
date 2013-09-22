f3ba1786fe6170c3eeeacc73e297afe249bf3aa2
Andrew Sun <andy.sun00@gmail.com>
2013-09-21 20:46:34 -0400

    Plugin boilerplate

---
+++ element-replacer.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: ElementReplacer
+ * Plugin URI: http://andrewsun.com/programs/elementreplacer/
+ * Description: Replaces elements in the page
+ * Version: 0.1
+ * Author: Andrew Sun
+ * Author URI: http://andrewsun.com/
+ * License: GPL3
+ */
