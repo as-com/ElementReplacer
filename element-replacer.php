3597a1ac54728f9152bd9cce22a84e347fc8dbe8
Andrew Sun <andy.sun00@gmail.com>
2013-09-21 20:49:25 -0400

    Let's do something more exciting...

--- element-replacer.php
+++ element-replacer.php
@@ -8,3 +8,17 @@
  * Author URI: http://andrewsun.com/
  * License: GPL3
  */
+
+function ereplacer_callback($buffer) {
+  // modify buffer here, and then return the updated code
+  return $buffer;
+}
+
+function ereplacer_buffer_start() { ob_start("ereplacer_callback"); }
+
+function ereplacer_buffer_end() { ob_end_flush(); }
+
+add_action('wp_head', 'ereplacer_buffer_start');
+add_action('wp_footer', 'ereplacer_buffer_end');
+
+?>
